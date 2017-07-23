0af0cee8073663d3e451274d60af3bc454d116a5
Huka NGUYEN <hungnlk@hukacode.com>
2017-07-23 17:14:03 +0700

    Init project structure

---
+++ elibrary-web-app/src/main/java/com/hukacode/elibrary/ElibraryWebApplication.java
@@ -0,0 +1,12 @@
+package com.hukacode.elibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElibraryWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ElibraryWebApplication.class, args);
+  }
+}
