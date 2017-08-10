7c4c8c44a76bf3222da89b6f342a1558155edbe6
nguyen ngoc diep <ngocdiep02tk@gmail.com>
2017-08-10 16:45:08 +0700

    add abc variable

--- elibrary-web-app/src/main/java/com/hukacode/elibrary/ElibraryWebApplication.java
+++ elibrary-web-app/src/main/java/com/hukacode/elibrary/ElibraryWebApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ElibraryWebApplication {
 
+  int abc;
+
   public static void main(String[] args) {
     SpringApplication.run(ElibraryWebApplication.class, args);
   }
